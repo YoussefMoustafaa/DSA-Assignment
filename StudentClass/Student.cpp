2d0226ad67ff7e19c31c42e615806e8d7a11f58c
mr-70da <114266893+mr-70da@users.noreply.github.com>
2024-04-23 17:45:49 +0200

    Update Student.cpp

--- StudentClass/Student.cpp
+++ StudentClass/Student.cpp
@@ -1,21 +1,86 @@
 #include <iostream>
-#include "../StudentClass/Student.h"
+
+#include "Student.h"
 
 using namespace std;
 
-Student::Student(string studName, string studID, int StudGpa) {
+Student::Student(string studName, string studID, double StudGpa)
+{
     this->name = studName;
     this->id = studID;
     this->gpa = StudGpa;
 }
-
-ostream& operator << (ostream& out, const Student& st) {
-    out << "Name: " << st.name << endl;
-    out << "ID: " << st.id << endl;
-    out << "GPA: " << st.gpa << endl;
-    return out;
+Student ::Student(Student &student)
+{
+    this->name = student.name;
+    this->id = student.id;
+    this->gpa = student.gpa;
 }
 
-bool Student::operator< (const Student& st) {
+Student::Student()
+{
+}
+
+Student :: Student(const Student& other) : name(other.name), id(other.id), gpa(other.gpa) {}
+
+ostream &operator<<(ostream &out, const Student &st)
+{
+    out << "Name: " << st.getName() << endl;
+    out << "ID: " << st.getID() << endl;
+    out << "GPA: " << st.getGPA() << endl;
+    return out;
+}
+istream& operator>>(istream& in, Student& st){
+    in >> st.name >> st.id >> st.gpa;
+    return in;
+}
+
+void Student::setName(string studName)
+{
+    this->name = studName;
+}
+
+void Student::setID(string studID)
+{
+    this->id = studID;
+}
+void Student::setGPA(double studGpa)
+{
+    this->gpa = studGpa;
+}
+
+string Student::getName() const
+{
+    return this->name;
+}
+
+string Student::getID() const
+{
+    return this->id;
+}
+
+double Student::getGPA() const
+{
+    return this->gpa;
+}
+
+bool Student::operator<(const Student &st)
+{
     return this->name < st.name;
+}
+
+bool studentNameComparer(Student &S1, Student &S2)
+{
+    return S1.getName() < S2.getName();
+}
+bool studentNameComparerOrEqual(Student &S1, Student &S2)
+{
+    return S1.getName() <= S2.getName();
+}
+bool studentGPAComparer(Student &S1, Student &S2)
+{
+    return S1.getGPA() < S2.getGPA();
+} bool studentGPAComparerOrEqual(Student &S1, Student &S2)
+{
+    return S1.getGPA() <=S2.getGPA();
 }
